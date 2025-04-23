20b9371ab406892b4e435d356518eb263e23689c
prabhatbhusal <prabhatbhusal777@gmail.com>
2025-04-23 09:47:08 +0545

    Adding Files

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
-
+import { navItems } from '../lib/constants/data';
+import  Link  from 'next/link';
 const Navbar = () => {
   return (
     <div>
-      
+      <div className='flex items-center '>
+        {navItems.map((link) => (
+          <Link key={link.id} href={link.url}>{link.title}</Link>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
