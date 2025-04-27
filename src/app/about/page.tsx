c90c3af15b464027c5f761121112a07f45e9aeab
prabhatbhusal <prabhatbhusal777@gmail.com>
2025-04-28 00:49:40 +0545

    Adding Files

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import PageBanner from '@/components/PageBanner'
+import { aboutdata } from '@/lib/constants/data'
 const page = () => {
   return (
     <main>
@@ -11,6 +12,20 @@ const page = () => {
         content="Whether it’s a flat tire, a dead battery, or you’re simply locked out, we’ve got your back—day or night. With Polk Roadside Assistance, help is just a call away. Stranded? Our expert team is always on call to get you back on track—quickly, safely, and hassle-free."
         service="ALWAYS HERE, ALWAYS READY"
       />
+      <div className="flex flex-col text-white">
+        <div className="flex-col bg-center bg-[url(/images/time.png)] h-[128.51vh] bg-cover ">
+          {aboutdata.map((about, idx) => (
+            <div key={idx} className="flex flex-row-reverse">
+              <div className=" flex flex-col border p-20 w-auto rouunded">
+                <h6 className="text-3xl">{about.content}</h6>
+              </div>
+              <div>
+                <h2 className=" text-primary lg:text-5xl md:">{about.title}</h2>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
--- src/lib/constants/data.ts
+++ src/lib/constants/data.ts
@@ -292,3 +292,21 @@ export const services: serviceprop[] = [
     link: "/services/airdelivery",
   },
 ];
+
+export const aboutdata = [
+  {
+    title: "MISSION",
+    content:
+      "Our Mission is Simple-To deliver fast, dependable, and top-quality roadside assistance services that get you back on the road safely. We strive to be the trusted first call for anyone facing a roadside emergency—providing quick, effective, and stress-free solutions anytime, anywhere.",
+  },
+  {
+    title: "VISION",
+    content:
+      "Our Vision-To become the most trusted roadside assistance provider by setting the standard for rapid response, exceptional service, and customer-first care. We envision a future where every driver feels safe and supported on the road, no matter the situation.",
+  },
+  {
+    title: "OUR TEAM",
+    content:
+      "Behind every service call is a dedicated team of trained and experienced professionals who know how to handle all types of roadside challenges. From tire changes to towing, our experts are equipped with modern tools, in-depth knowledge, and a genuine commitment to helping you when you need it most.",
+  },
+];
