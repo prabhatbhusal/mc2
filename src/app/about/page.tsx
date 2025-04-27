089d6b6f576329dc7cc439ce6c7593ba0124b27b
prabhatbhusal <prabhatbhusal777@gmail.com>
2025-04-27 10:00:22 +0545

    Adding Files

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,9 +4,15 @@ import PageBanner from '@/components/PageBanner'
 const page = () => {
   return (
     <main>
-        <PageBanner heading1='' heading2='' info='' content='' service={''}/>
+      <PageBanner
+        heading1="Tailored Service"
+        heading2="-A Call Away"
+        info="No matter the situation, our expert team is ready to assist"
+        content="From flat tires to dead batteries, our trained technicians are ready 24/7 to get you back on the road safely and quickly. We serve Killeen, Copperas Cove, Temple, and nearby areas with reliable and affordable roadside assistance."
+        service="Our Roadside Services"
+      />
     </main>
-  )
+  );
 }
 
 export default page
