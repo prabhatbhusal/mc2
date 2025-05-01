3f742d4b654ac9aaf2689eceb548ce8982eb415a
prabhatbhusal <prabhatbhusal777@gmail.com>
2025-05-01 13:37:59 +0545

    Changes Commit

--- src/components/ServicesBanner.tsx
+++ src/components/ServicesBanner.tsx
@@ -5,27 +5,27 @@ import { Search } from 'lucide-react';
 const PageBanner:React.FC<props> = (prop) => {
   return (
     <main>
-      <div className="text-white flex flex-col h-[125vh] items-center font-medium  gap-90 bg-[url(/images/car.png)]  bg-fill bg-center bg-no-repeat bg-blend-overlay bg-neutral-500 ">
+      <div className="text-white flex flex-col h-[125vh] items-center font-medium gap-45 lg:gap-90 bg-[url(/images/car.png)]  bg-fill bg-center bg-no-repeat bg-blend-overlay bg-neutral-500 ">
         <div className="flex flex-col items-center justify-center py-52 gap-[24px]">
           <div className="flex flex-col items-center gap-[10px]">
             <p className="">24/7 ASSISTANCE</p>
             <hr className="w-15 text-center text-white" />
           </div>
 
-          <h2 className="lg:text-[92px] md:text-[50px] sm:text-[50px]  xs:text-5xl font-bold">
+          <h2 className="lg:text-[92px] md:text-[50px] text-[50px] text-center  font-bold">
             {prop.heading1}
           </h2>
-          <h1 className=" text-primary lg:text-[92px] md:text-4xl sm:text:3xl font-bold">
+          <h1 className=" text-primary lg:text-[92px] md:text-4xl text-[50px] text-center  font-bold">
             {prop.heading2}
           </h1>
-          <p className="font-light">{prop.info}</p>
+          <p className="font-light w-auto">{prop.info}</p>
         </div>
         <div className="flex flex-col items-center justify-center  gap-5 ">
           <div className="lg:text-[60px] md:text-4xl sm:text-3xl flex gap-5">
-            <h2 className="text-white lg:text-[92px] md:text-[50px] sm:text-[50px] xs:text-[50px]">
+            <h2 className="text-white lg:text-[92px] md:text-[50px] text-[50px] xs:text-[50px]">
               {prop.service1}
             </h2>
-            <h2 className="text-primary lg:text-[92px] md:text-[50px] sm:text-[50px] xs:text-[50px]">
+            <h2 className="text-primary lg:text-[92px] md:text-[50px] text-[50px] xs:text-[50px]">
               {prop.service2}
             </h2>
           </div>
